582775d5181162404f28eb8d95126f2d3d30fc3a
ceceppa <senesealessandro@gmail.com>
2022-11-29 22:57:25 +0000

    fix script

--- init-and-run-tests.sh
+++ init-and-run-tests.sh
@@ -4,7 +4,7 @@ set -e
 GODOT_VERSION=$1
 GUT_PARAMS=$2
 
-# Download Godot
+# # Download Godot
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_linux_headless.64.zip
 
 # Unzip it
@@ -31,16 +31,13 @@ cat $TEMP_FILE
 
 # Godot always exists with error 0, but we want this action to fail in case of errors
 
-NO_TEST_RAN=$(cat $TEMP_FILE | grep "No tests ran")
-
-if [ ! -z "$NO_TEST_RAN" ]
+if grep -q "No tests ran" "$TEMP_FILE";
 then
-  echo "No test ran. Please check your `gut_params`"
+  echo "No test ran. Please check your 'gut_params'"
   exit 1
 fi
 
-HAS_FAILED=$(cat $TEMP_FILE | grep " [Failed]: ")
-if [ ! -z "$HAS_FAILED" ]
+if  grep -q " [Failed]: " "$TEMP_FILE"
 then
   echo "One or more test have failed"
   exit 1
