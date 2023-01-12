38113a7c37d071981c91d594feb065e8f9f6054b
ceceppa <senesealessandro@gmail.com>
2023-01-12 08:20:10 +0000

    try to fix output not visible

--- init-and-run-tests.sh
+++ init-and-run-tests.sh
@@ -25,7 +25,7 @@ echo Running GUT tests using params:
 echo "  -> $GUT_PARAMS"
 
 TEMP_FILE=/tmp/gut.log
-/usr/local/bin/godot -d -s --path $PWD addons/gut/gut_cmdln.gd -gexit $GUT_PARAMS # > $TEMP_FILE 2>&1
+/usr/local/bin/godot -d -s --path $PWD addons/gut/gut_cmdln.gd -gexit $GUT_PARAMS 2>&1 | tee $TEMP_FILE
 
 echo "LOG Output:"
 cat $TEMP_FILE
